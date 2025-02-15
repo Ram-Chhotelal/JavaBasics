85f9cbbd52e7bc6d4041f7598ebb1b6063118fa3
krpsoftech <ram.ch4226@gmail.com>
2025-02-15 15:47:10 +0530

    This is the MultiplicationTable.

---
+++ src/BasicsJavaCode/MultiplicationTable.java
@@ -0,0 +1,18 @@
+package BasicsJavaCode;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int num = scanner.nextInt();
+
+        System.out.println("Multiplication table for" + " " + num + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + "x" + i + "=" + (num * i));
+        }
+        scanner.close();
+    }
+}
