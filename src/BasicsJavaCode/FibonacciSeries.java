7c763ba8dcf4b63fb49f8f1e685e34f2f5748ec0
krpsoftech <ram.ch4226@gmail.com>
2025-02-18 12:50:15 +0530

    This is the Fibonacci series.

---
+++ src/BasicsJavaCode/FibonacciSeries.java
@@ -0,0 +1,23 @@
+package BasicsJavaCode;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of terms: ");
+        int n = scanner.nextInt();
+
+        int first = 0, second = 1;
+        System.out.print("Fibonacci Series upto " + n + "terms: ");
+
+        for (int i = 0; i <= n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+        scanner.close();
+    }
+}
